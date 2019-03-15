480c29b5fbdab9b5c81eabc5aec0b75c7404c6ab
Nayomi Morita <nayomi.morita@gmail.com>
2019-03-15 14:21:20 -0700

    add javascript for index

--- src/main.js
+++ src/main.js
@@ -5,13 +5,15 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Input, lifeLeft, dayAge, daysLeft, yourPlanetAge, timeLeftOnYourPlanet } from './backEnd.js';
 
-// $(function(){
-//   $(".submit").click(function(){
-//     event.preventDefault();
-//     const newInput = new Input(age, planet);
-//     const planet = $(".planet").val();
-//     const age = $(".age").val();
-//     newInput.planetSelector();
-//     $(".output").html(planetAge);
-//   });
-// });
+$(function(){
+  $(".submit").click(function(){
+    console.log(alert("yo"))
+    event.preventDefault();
+    const planet = $(".planet").val();
+    const age = $(".age").val();
+    const newInput = new Input(age, planet);
+    let plonet = yourPlanetAge(age, planet);
+    $("#output").text(plonet);
+    console.log(alert(newInput(age, planet)))
+  });
+});
