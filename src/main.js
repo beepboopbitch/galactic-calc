8b5878a0352f77e903e9ead461900f60fdf73305
Nayomi Morita <nayomi.morita@gmail.com>
2019-03-15 08:56:27 -0700

    add new file

--- src/main.js
+++ src/main.js
@@ -1,19 +1,1 @@
-import './css/styles.css';
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Input, yearCalc, monthCalc, daysCalc } from './backEnd.js';
 
-$(function(){
-  $(".submit").click(function(){
-    const day = $(".day").val();
-    const month = $(".month").val();
-    const year = $(".year").val();
-    const newInput = new Input(month, day, year);
-    const yearDays = yearCalc(newInput);
-    const monthDays = monthCalc(newInput);
-    let days = daysCalc(newInput, yearDays, monthDays);
-    $(".return").text(days);
-  });
-});
